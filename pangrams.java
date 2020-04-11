9fb1a378319528f120f842dad8b9a5b0bdf9d9ab
Technical hub <62637097+technicalhub9@users.noreply.github.com>
2020-04-11 11:14:42 +0530

    Add files via upload

---
+++ pangrams.java
@@ -0,0 +1,34 @@
+public class Pangrams 
+{
+   public static void main(String[] args) 
+	{
+      String str = "Brown jars prevented the mixture from freezing too quickly"; 
+
+      boolean[] alphaList = new boolean[26]; 
+
+      int index = 0;
+      int flag = 1;
+      for (int i = 0; i < str.length(); i++) 
+	{
+         if ( str.charAt(i) >= 'A' && str.charAt(i) <= 'Z') 
+		{
+            	index = str.charAt(i) - 'A'; 
+         	}
+	else if( str.charAt(i) >= 'a' &&  str.charAt(i) <= 'z') 
+	{
+            index = str.charAt(i) - 'a'; 
+         }
+         alphaList[index] = true; 
+      }
+      for (int i = 0; i <= 25; i++) 
+	{
+         if (alphaList[i] == false)
+            flag = 0;
+        }
+      System.out.print("String: " + str);
+      if (flag == 1)
+         System.out.print("\nThe above string is a pangram.");
+      else
+         System.out.print("\nThe above string is not a pangram.");
+   }
+}
